f27910b7fff21aba843e7ad8cc4e93b4702b7fe5
Christoph <christophhummer@gmail.com>
2026-02-08 13:01:11 +0100

    Update chroot-script.sh
    - add Midnight Commander

--- chroot-script.sh
+++ chroot-script.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends --no-i
   isc-dhcp-client acpi-support-base nginx tree samba xz-utils rsyslog ntpsec kbd console-data hdparm smartmontools at netcat-openbsd cryptsetup \
   net-tools sysstat pciutils usbutils nut-server beep inotify-tools libjson-glib-1.0-0 libxml2 libpciaccess0 open-iscsi tgt dos2unix dosfstools \
   iptables libpixman-1-0 libspice-server1 libusbredirparser1 dnsmasq lm-sensors gnutls-bin cron rsync fuse3 nfs-kernel-server nfs-common libpng16-16 \
-  wireguard-tools bash-completion libxml2-utils yq ethtool attr xattr psmisc
+  wireguard-tools bash-completion libxml2-utils yq ethtool attr xattr psmisc mc
 
 DEB_APPS=$(ls -1 /tmp/applications/*.deb)
 for deb_app in $DEB_APPS; do
